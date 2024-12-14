5ac22d5d154f54e9e32d8d0dd75d415cc95a09db
Job <pulachecarrenojob837@gmail.com>
2024-12-13 22:07:25 -0500

    add modal with the content

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,20 @@
-export default function Modal() {
+import { useNavigate } from 'react-router-dom'
+
+import classes from '../assets/css/Modal.module.css'
+
+export default function Modal({ children }) {
+  const navigate = useNavigate();
+
+  function closeHandler(){
+    navigate('..');
+  }
+
   return (
-    <div>Modal</div>
+    <>
+      <div className={classes.backdrop} onClick={closeHandler} />
+      <dialog open className={classes.modal}>
+        {children}
+      </dialog>
+    </>
   )
 }
