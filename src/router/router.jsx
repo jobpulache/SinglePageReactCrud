e876c49162c6dcdc54c7172dc3b831549d136bff
Job <pulachecarrenojob837@gmail.com>
2024-12-13 21:48:46 -0500

    add pages dashboard, register and component modal

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter}from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../components/layout';
 import Home from '../pages/Home';
 import Products from '../pages/Products';
@@ -6,43 +6,47 @@ import FormNewProduct from '../components/FormNewProduct';
 import ProductDetails from '../components/ProductDetails';
 import { productsLoader } from '../services/loaders/ProductsLoader';
 import { ProductsDetailsLoader } from '../services/loaders/ProductDetailsLoader';
-import {NewProductAction} from '../services/actions/NewProductAction';
+import { NewProductAction } from '../services/actions/NewProductAction';
 import NotFound from '../pages/NotFound';
 import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+import Register from '../pages/Register';
 
-export const router = createBrowserRouter([{
-    path: '/',
-    element: <Layout/>,
-    children:[
-        {
-            path: '/',
-            element: <Home/>
-        },
-        {
-            path: '/products',
-            element:<Products/>,
-            loader: productsLoader,
-            children: [
-                { path: '/products/create-product', element: <FormNewProduct/>, action: <NewProductAction/>},
-                {path: '/products/:id', element: <ProductDetails/>, loader:ProductsDetailsLoader},
-                {
-                    path: '/customers',
-                    element: <Layout/>,
-                },
-                {
-                    path: '*',
-                    element: <NotFound/>
-                }
-            ]
-        },
-        {
-            path: '/login',
-            element: <Login/>
-        },
-       
-        {
-            path: '*',
-            element: <NotFound/>
-        }
-    ]
-}]);
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: '/products',
+        element: <Products />,
+        loader: productsLoader,
+        children: [
+          { path: '/products/create-product', element: <FormNewProduct />, action: NewProductAction },
+          { path: '/products/:id', element: <ProductDetails />, loader: ProductsDetailsLoader}
+                    
+                ],
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/register',
+                element: <Register />,
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />,
+            },
+            {
+                path: '*',
+                element: <NotFound />,
+            },
+        ],
+    },
+]);
--- src/services/actions/NewProductAction.js
+++ src/services/actions/NewProductAction.js
@@ -3,7 +3,7 @@ import { redirect } from "react-router-dom";
 export async function NewProductAction({request}){
     const formData = await request.formData();
     const productData = Object.fromEntries(formData.entries());
-    await fetch('http://localhost:8080/products', {
+    await fetch('http://localhost:5173/products', {
         method: 'POST',
         body: JSON.stringify(productData),
         headers:{
