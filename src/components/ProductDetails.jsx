e6c0c30ac75abafd98800e787c325b520703dc38
Job <pulachecarrenojob837@gmail.com>
2024-12-13 22:58:12 -0500

    add content in header

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,9 +1,34 @@
-import Modal from "./Modal";
+import { useLoaderData, Link } from 'react-router-dom';
+
+import Modal from '../components/Modal'
+import classes from '../assets/css/ProductDetails.module.css'
 
 export default function ProductDetails() {
+  const product = useLoaderData();
+  
+  if (!product) {
+    return (
+      <Modal>
+        <main className={classes.details}>
+          <h1>Could not find post</h1>
+          <p>Unfortunately, the requested post could not be found.</p>
+          <p>
+            <Link to=".." className={classes.btn}>
+              Okay
+            </Link>
+          </p>
+        </main>
+      </Modal>
+    );
+  }
   return (
-    <>
-    <Modal/>
-    </>
-  )
+    <Modal>
+      <main className={classes.details}>      
+        <p className={classes.author}>ID: {product.id}</p>
+        <p className={classes.text}>Title: {product.title}</p>
+        <p className={classes.text}>Description: {product.description}</p>
+      </main>
+    </Modal>
+  );
+  
 }
